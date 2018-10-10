dc5964745ca1bd685ce2695a7f0f123490795df6
Michael LeGrange <mlegrange15@att.net>
2018-10-09 20:02:21 -0500

    added total score and clicked citites to state

--- src/components/AppContainer/AppContainer.js
+++ src/components/AppContainer/AppContainer.js
@@ -10,53 +10,78 @@ import cities from "../../cities.json";
 import "./AppContainer.css";
 
 class AppContainer extends React.Component {
+  state = {
+    cities,
+    score: 0,
+    totalscore: 0,
+    clickedCities: []
+  };
 
-    state = {
-        cities,
-        score: 0,
-        totalscore: 0
+  handleClick = id => {
+    this.shuffle(this.state.cities);
+
+    let alreadyClicked = this.state.clickedCities;
+    console.log("Already clicked = " , alreadyClicked.includes(id));
+
+    if ( alreadyClicked === false) {
+
+        this.state.clickedCities.push(id);
+        console.log("this is the clicked cities array: ", this.state.clickedCities);
+        console.log("this is shuffled cities: ", this.state.cities);
+        this.setState({ cities, score: this.state.score + 1 });
+    } else {
+
+        console.log("Add logic to reset the game and check if the total high score was beaten!");
+        
     }
+  };
 
-    handleClick = (id) => {
-        const cities = this.state.cities.filter(city => city.id !== id);
-        this.setState({ cities, score: this.state.score + 1  })        
-    };
-
-    render() {
-        return (
-            <Wrapper>
-                <Navbar 
-                    score={this.state.score}
-                    totalscore={this.state.totalscore}
-                />
-                <Header></Header>
-                <Row>
-                    <Col size="md-12">
-                    </Col>
-                </Row>
-                <Container>
-                    <Row>
-                        {this.state.cities.map((f, i) => {
-                            return <Col size="md-4"
-                                key={f.id}
-                                id={f.id}
-                            >
-                                <CityCard
-                                    name={f.name}
-                                    image={f.image}
-                                    nickname={f.nickname}
-                                    location={f.location}
-                                    key={f.id}
-                                    id={f.id}
-                                    handleClick={this.handleClick}
-                                />
-                            </Col>
-                        })}
-                    </Row>
-                </Container>
-            </Wrapper>
-        )
+  shuffle = array => {
+    let control = array.length,
+      temp,
+      index;
+    while (control > 0) {
+      index = Math.floor(Math.random() * control);
+      control--;
+      temp = array[control];
+      array[control] = array[index];
+      array[index] = temp;
     }
-};
+    return array;
+  };
+
+  handleTotalScore = () => {};
+
+  render() {
+    return (
+      <Wrapper>
+        <Navbar score={this.state.score} totalscore={this.state.totalscore} />
+        <Header />
+        <Row>
+          <Col size="md-12" />
+        </Row>
+        <Container>
+          <Row>
+            {this.state.cities.map((f, i) => {
+              return (
+                <Col size="md-2" key={f.id} id={f.id}>
+                  <CityCard
+                    name={f.name}
+                    image={f.image}
+                    nickname={f.nickname}
+                    location={f.location}
+                    key={f.id}
+                    id={f.id}
+                    handleClick={this.handleClick}
+                  />
+                </Col>
+              );
+            })}
+          </Row>
+        </Container>
+      </Wrapper>
+    );
+  }
+}
 
 export default AppContainer;
